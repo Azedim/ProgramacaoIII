157c133176c657dd4747559fe04fd051ba714bed
Azedim <gabrielbauer2010@gmail.com>
2019-04-05 15:47:25 -0300

    aula acabada

--- formulario/main.js
+++ formulario/main.js
@@ -1,6 +1,8 @@
 console.log("Olá Mundo!"); //printa olá mundo no console (F12 e console fica la em baixo)
 //alert("Olá mundo!"); //printa assim que entra na pagina
-var bSoma = document.querySelector('.soma');
+var bSoma = document.querySelector('.soma'); //variavel global ou estatica, pode estar no fim do programa, mas pode participar de uma função no inicio
+
 bSoma.addEventListener('click',function(){ //addEventListener('evento',função(){oque vai acontecer quando o evento for realizado})
-    alert("clicou");
+    let v1 = document.querySelector('.valor1');//let é uma variavel local só existe dentro da sua função
+    alert(v1.value); //value = valor
 });
