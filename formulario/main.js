82e1ed3d13679dc9dcf9d421c98b9541be50cba2
Azedim <gabrielbauer2010@gmail.com>
2019-04-05 15:33:15 -0300

    js incluido

---
+++ formulario/main.js
@@ -0,0 +1,6 @@
+console.log("Olá Mundo!"); //printa olá mundo no console (F12 e console fica la em baixo)
+//alert("Olá mundo!"); //printa assim que entra na pagina
+var bSoma = document.querySelector('.soma');
+bSoma.addEventListener('click',function(){ //addEventListener('evento',função(){oque vai acontecer quando o evento for realizado})
+    alert("clicou");
+});
