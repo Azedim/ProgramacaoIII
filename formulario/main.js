34fa9f56e23335ce790cb5c2cafcbd88462f3bc1
Azedim <gabrielbauer2010@gmail.com>
2019-04-12 14:05:32 -0300

    soma pronta

--- formulario/main.js
+++ formulario/main.js
@@ -4,7 +4,11 @@ var bSoma = document.querySelector('.soma'); //variavel global ou estatica, pode
 
 bSoma.addEventListener('click',function(e){ //addEventListener('evento',função(){oque vai acontecer quando o evento for realizado})
     let v1 = document.querySelector('.valor1');//let é uma variavel local só existe dentro da sua função
-    alert(v1.value); //value = valor, printa o valor de v1
-    e.preventDefault();
-    console.log(e);
+    let v2 = document.querySelector('.valor2');
+    let soma = parseInt(v1.value)+parseInt(v2.value);
+    document.querySelector(".resultado").innerHTML = "O resultado é: " + soma;
+
+    //alert(v1.value); //value = valor, printa o valor de v1
+    e.preventDefault(); //não recarrega a pagina, como é padrão
+    //console.log(e.target); printa no console o evento disparado
 });
