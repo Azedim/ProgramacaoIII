f24924aeea086d2406ca7678d39a0e9a6541dd93
Azedim <gabrielbauer2010@gmail.com>
2019-04-12 13:45:59 -0300

    java update

--- formulario/main.js
+++ formulario/main.js
@@ -2,7 +2,9 @@ console.log("Olá Mundo!"); //printa olá mundo no console (F12 e console fica l
 //alert("Olá mundo!"); //printa assim que entra na pagina
 var bSoma = document.querySelector('.soma'); //variavel global ou estatica, pode estar no fim do programa, mas pode participar de uma função no inicio
 
-bSoma.addEventListener('click',function(){ //addEventListener('evento',função(){oque vai acontecer quando o evento for realizado})
+bSoma.addEventListener('click',function(e){ //addEventListener('evento',função(){oque vai acontecer quando o evento for realizado})
     let v1 = document.querySelector('.valor1');//let é uma variavel local só existe dentro da sua função
-    alert(v1.value); //value = valor
+    alert(v1.value); //value = valor, printa o valor de v1
+    e.preventDefault();
+    console.log(e);
 });
