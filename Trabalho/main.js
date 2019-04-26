d0dc7422a2f335f4cd78defab22052843e80736a
Azedim <gabrielbauer2010@gmail.com>
2019-04-26 15:00:54 -0300

    java update

--- Trabalho/main.js
+++ Trabalho/main.js
@@ -1,2 +1,14 @@
-var chBox = document.querySelector("#light");
+var Radio = document.querySelector("#light");
+var Radio2 = document.querySelector("#dark");
+Radio.addEventListener("click",function(e){
+    document.getElementById("contain").style.background = "#001";
+    document.getElementById("contain").style.color = "white";
 
+    
+})
+
+Radio2.addEventListener("click",function(e){
+    document.getElementById("contain").style = 'background-color : 001';
+
+    
+})
