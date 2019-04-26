e3738e75c908442ee72f2d66a35fcce610a89e7c
Azedim <gabrielbauer2010@gmail.com>
2019-04-26 15:44:34 -0300

    end of day

--- Trabalho/main.js
+++ Trabalho/main.js
@@ -1,14 +1,15 @@
 var Radio = document.querySelector("#light");
 var Radio2 = document.querySelector("#dark");
 Radio.addEventListener("click",function(e){
-    document.getElementById("contain").style.background = "#001";
-    document.getElementById("contain").style.color = "white";
+    document.getElementById("contain").style.background = "white";
+    document.getElementById("contain").style.color = "black";
 
     
 })
 
 Radio2.addEventListener("click",function(e){
-    document.getElementById("contain").style = 'background-color : 001';
+    document.getElementById("contain").style.background = "#001";
+    document.getElementById("contain").style.color = "white";
 
     
 })
